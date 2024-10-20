37a2ffac14ebda1d53cdaac4e274843e0e715573
Douglas Ciole <douglasciole@gmail.com>
2024-10-20 14:26:11 -0700

    Create the basic layout

--- app/components/AddToCart.jsx
+++ app/components/AddToCart.jsx
@@ -3,10 +3,7 @@ import React from "react";
 
 const AddToCart = () => {
 	return (
-		<button
-			onClick={() => console.log("Test")}
-			className="border border-amber-400 p-5 hover:text-white hover:bg-gray-950 rounded"
-		>
+		<button onClick={() => console.log("Test")} className="btn btn-primary">
 			Add to the cart
 		</button>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
-import ProductCard from "./components/ProductCard";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import EventCard from "./components/EventCard";
 
 export default function Home() {
 	return (
-		<main className="flex items-center justify-center min-h-screen">
-			<div className="border border-amber-400 p-5 hover:text-white hover:bg-gray-950 cursor-pointe rounded">
-				<Link href="/users">Users</Link>
+		<main>
+			<NavBar />
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center">
+				<div className="card bg-base-100 w-80 shadow-xl">
+					<EventCard />
+				</div>
+				<div className="card bg-base-100 w-80 shadow-xl">
+					<EventCard />
+				</div>
+				<div className="card bg-base-100 w-80 shadow-xl">
+					<EventCard />
+				</div>
+				<div className="card bg-base-100 w-80 shadow-xl">
+					<EventCard />
+				</div>
+				<div className="card bg-base-100 w-80 shadow-xl">
+					<EventCard />
+				</div>
+				<div className="card bg-base-100 w-80 shadow-xl">
+					<EventCard />
+				</div>
 			</div>
-			<ProductCard />
+			<Footer />
 		</main>
 	);
 }
